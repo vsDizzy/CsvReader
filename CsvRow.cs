5e51a9e821894622807f0410ac28372e7e8adb30
Dizzy <todizzy@gmail.com>
2015-04-21 18:47:30 +0300

    `

---
+++ CsvRow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HoldingWebJob.Csv
+{
+	internal class CsvRow
+	{
+		private readonly string[] _cols;
+		private readonly string[] _data;
+
+		public CsvRow(string[] cols, string[] data)
+		{
+			_cols = cols;
+			_data = data;
+		}
+
+		public string this[int index]
+		{
+			get { return _data[index].Trim('"'); }
+		}
+
+		public string this[string column]
+		{
+			get
+			{
+				var index = Array.IndexOf(_cols, column);
+				return this[index];
+			}
+		}
+	}
+}
---
+++ CsvTable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace HoldingWebJob.Csv
+{
+	internal class CsvTable
+	{
+		private readonly IEnumerable<string> _lines;
+
+		public CsvTable(string filename)
+		{
+			_lines = File.ReadLines(filename);
+		}
+
+		public string[] Cols { get; private set; }
+
+		public IEnumerable<CsvRow> Rows
+		{
+			get
+			{
+				var le = _lines.GetEnumerator();
+				if (le.MoveNext())
+				{
+					Cols = ReadLine(le.Current);
+				}
+				while (le.MoveNext())
+				{
+					yield return new CsvRow(Cols, ReadLine(le.Current));
+				}
+			}
+		}
+
+		private string[] ReadLine(string line)
+		{
+			return line.Split(',');
+		}
+	}
+}
